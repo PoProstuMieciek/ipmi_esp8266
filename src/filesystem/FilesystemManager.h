97439c95af0beee6eae8929da7aceb24577a7811
Maciej Opaliński <poprostumieciek@gmail.com>
2021-05-01 12:00:04 +0200

    Add FilesystemManager class

---
+++ src/filesystem/FilesystemManager.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <Arduino.h>
+#include <LittleFS.h>
+#include <vector>
+
+#include "system/SystemManager.h"
+#include "logger/Logger.h"
+
+class FilesystemManager
+{
+    public:
+        SystemManager sys;
+        Logger logger;
+        
+        void begin(SystemManager &sys);
+
+        String getFileContent(String path);
+        std::vector<String> listDirectory(String dir_path);
+};
