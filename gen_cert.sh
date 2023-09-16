4194d9b4d66365b2585575a9c021e7c3457fb96b
Maciej Opaliński <poprostumieciek@gmail.com>
2023-09-16 14:28:55 +0200

    Update script to generate certs

--- gen_cert.sh
+++ gen_cert.sh
@@ -1,4 +1,19 @@
-cd ./data/certs/
-rm esp8266.key.pem esp8266.cert.pem
-openssl genrsa -out esp8266.key.pem 1024
-openssl req -new -x509 -key esp8266.key.pem -out esp8266.cert.pem -days 90 -subj /C=PL/CN=ipmi.mieciuu.live/emailAddress=poprostumieciek@gmail.com
+read -p "Domain: " domain
+read -p "Email: " email
+read -p "Days: " days
+
+mkdir -p ./data/certs
+
+rm ./data/certs/key.pem ./data/certs/cert.pem
+
+openssl genrsa \
+    -out ./data/certs/key.pem \
+    1024
+
+openssl req \
+    -new \
+    -x509 \
+    -key ./data/certs/key.pem \
+    -out ./data/certs/cert.pem \
+    -days "$days" \
+    -subj /CN=$domain/emailAddress=$email
